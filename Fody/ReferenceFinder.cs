4f38df0977078193401a5c30e81a80b763fc0788
Simon Cropp <simon.cropp@gmail.com>
2013-12-08 21:04:34 +1100

    better support for PCL

--- Fody/ReferenceFinder.cs
+++ Fody/ReferenceFinder.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Mono.Cecil;
 
 public partial class ModuleWeaver
@@ -6,50 +7,52 @@ public partial class ModuleWeaver
 
     public void FindCoreReferences()
     {
-        var assemblyResolver = ModuleDefinition.AssemblyResolver;
-        var msCoreLibDefinition = assemblyResolver.Resolve("mscorlib");
-        var msCoreTypes = msCoreLibDefinition.MainModule.Types;
+        var coreTypes = new List<TypeDefinition>();
+        AppendTypes("mscorlib", coreTypes);
+        AppendTypes("System.IO", coreTypes);
+        AppendTypes("System.Runtime", coreTypes);
+        AppendTypes("System.Reflection", coreTypes);
 
-        var textReaderTypeDefinition = msCoreTypes.First(x => x.Name == "TextReader");
+        var textReaderTypeDefinition = coreTypes.First(x => x.Name == "TextReader");
         ReadToEndMethod = ModuleDefinition.Import(textReaderTypeDefinition.Find("ReadToEnd"));
 
-        var exceptionTypeDefinition = msCoreTypes.First(x => x.Name == "Exception");
+        var exceptionTypeDefinition = coreTypes.First(x => x.Name == "Exception");
 		ExceptionConstructorReference = ModuleDefinition.Import(exceptionTypeDefinition.Find(".ctor", "String"));
 
-        var stringTypeDefinition = msCoreTypes.First(x => x.Name == "String");
+        var stringTypeDefinition = coreTypes.First(x => x.Name == "String");
 		ConcatReference = ModuleDefinition.Import(stringTypeDefinition.Find("Concat", "String", "String", "String"));
 
         DisposeTextReaderMethod = ModuleDefinition.Import(textReaderTypeDefinition.Find("Dispose"));
-        var streamTypeDefinition = msCoreTypes.First(x => x.Name == "Stream");
+        var streamTypeDefinition = coreTypes.First(x => x.Name == "Stream");
         DisposeStreamMethod = ModuleDefinition.Import(streamTypeDefinition.Find("Dispose"));
         StreamTypeReference = ModuleDefinition.Import(streamTypeDefinition);
-        var streamReaderTypeDefinition = msCoreTypes.First(x => x.Name == "StreamReader");
+        var streamReaderTypeDefinition = coreTypes.First(x => x.Name == "StreamReader");
         StreamReaderTypeReference = ModuleDefinition.Import(streamReaderTypeDefinition);
         StreamReaderConstructorReference = ModuleDefinition.Import(streamReaderTypeDefinition.Find(".ctor","Stream"));
-        var assemblyTypeDefinition = msCoreTypes.First(x => x.Name == "Assembly");
+        var assemblyTypeDefinition = coreTypes.First(x => x.Name == "Assembly");
         AssemblyTypeReference = ModuleDefinition.Import(assemblyTypeDefinition);
         GetExecutingAssemblyMethod = ModuleDefinition.Import(assemblyTypeDefinition.Find("GetExecutingAssembly"));
         GetManifestResourceStreamMethod = ModuleDefinition.Import(assemblyTypeDefinition.Find("GetManifestResourceStream", "String"));
     }
 
+    void AppendTypes(string name, List<TypeDefinition> coreTypes)
+    {
+        var definition = AssemblyResolver.Resolve(name);
+        if (definition != null)
+        {
+            coreTypes.AddRange(definition.MainModule.Types);
+        }
+    }
+
 	public MethodReference ConcatReference;
-
 	public MethodReference ExceptionConstructorReference;
-
 	public MethodReference DisposeStreamMethod;
-
     public MethodReference DisposeTextReaderMethod;
-
     public MethodReference ReadToEndMethod;
-
     public MethodReference StreamReaderConstructorReference;
-
     public TypeReference StreamTypeReference;
     public TypeReference StreamReaderTypeReference;
-
     public TypeReference AssemblyTypeReference;
-
     public MethodReference GetManifestResourceStreamMethod;
-
     public MethodReference GetExecutingAssemblyMethod;
 }
